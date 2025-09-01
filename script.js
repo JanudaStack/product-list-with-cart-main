6753c46d855b0e1bc9d0890faf18b010f19f2548
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-09-02 02:00:16 +0530

    fix: streamline cart item addition and improve remove button styling

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-const dessertTitle = document.querySelectorAll('.dessert-title').textContent;
-const itemPrice = document.querySelectorAll('.price-tag').textContent;
-const products = document.querySelectorAll('recipe-card');
-
 const addToCartBtn = document.querySelectorAll('.add-to-cart-btn');
+const cartItems = document.querySelector('.cart-items');
 
 addToCartBtn.forEach((btn) => {
     btn.addEventListener('click', (event) => {
         const itemCard = event.target.closest('.recipe-card');
-        const dessertTitle =
-            itemCard.querySelector('.dessert-title').textContent;
-        const priceTag = itemCard.querySelector('.price-tag').textContent;
-
-        const cartItem = document.querySelector('.cart-items');
+        const itemTitle = itemCard.querySelector('.dessert-title').textContent;
+        const itemPrice = itemCard.querySelector('.price-tag').textContent;
 
         const item = document.createElement('div');
         item.classList.add('item');
 
-        item.innerHTML = `<h3 class="cart-item-title">${dessertTitle}</h3>`;
+        item.innerHTML = `
+        <h3 class="cart-item-title">${itemTitle}</h3>
+         <div class="price-container">
+            <span class="item-quantity">1</span>
+             <span class="item-price">${itemPrice}</span>
+             <span class="quantity-price"></span>
+             <button class="remove-item-btn"></button>
+         </div>`;
 
-        const priceContainer = document.createElement('div');
-        priceContainer.classList.add('price-container');
-
-        const itemQuantity = 1;
-
-        priceContainer.innerHTML = `
-                                <span class="item-quantity">${itemQuantity}</span>
-                                <span class="item-price">${priceTag}</span>
-                                <span class="quantity-price"></span>
-                                <button class="remove-item-btn"><img/></button>`;
-
-        item.appendChild(priceContainer);
-        cartItem.appendChild(item);
-
-        console.log('button works', e.target.dessertTitle);
+        cartItems.appendChild(item);
     });
 });
 
-const removeItem = document.querySelectorAll('.remove-item-btn');
-
-removeItem.forEach((removeBtn) => {
-    removeBtn.addEventListener('click', (e) => {
-        const deleteItem = e.target.closest('.item');
-        deleteItem.style.display = 'none';
-        console.log('hello');
-    });
+cartItems.addEventListener('click', (event) => {
+    if (event.target.classList.contains('remove-item-btn')) {
+        const itemToRemove = event.target.closest('.item');
+        itemToRemove.remove();
+    }
 });
